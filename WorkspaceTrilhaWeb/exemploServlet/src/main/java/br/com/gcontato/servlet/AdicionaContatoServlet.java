94c5e6367fbf18dbb001d1f089feff2ccc3961ba
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-08-19 21:34:18 -0300

    OT 10 servlets BackEnd finalizada

--- WorkspaceTrilhaWeb/exemploServlet/src/main/java/br/com/gcontato/servlet/AdicionaContatoServlet.java
+++ WorkspaceTrilhaWeb/exemploServlet/src/main/java/br/com/gcontato/servlet/AdicionaContatoServlet.java
@@ -1,5 +1,6 @@
 package br.com.gcontato.servlet;
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,8 +22,31 @@ public class AdicionaContatoServlet extends HttpServlet{
 	
 		throws ServletException, IOException{
 		
+		PrintWriter out = response.getWriter();
+		String nome = request.getParameter("nome");
+		String fone = request.getParameter("fone");
+		String email = request.getParameter("email");
+		
+		out.println("<h1>Nome: </h1>");
+		out.println(nome);
+		out.println("<h1>Telefone: </h1>");
+		out.println(fone);
+		out.println("<h1>Email: </h1>");
+		out.println(email);
+		
 	}
-			
 	
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response)
+		throws ServletException, IOException{
+		
+	}
+	
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response)
+		throws ServletException, IOException{
+		
+	}
+				
 	
 }
