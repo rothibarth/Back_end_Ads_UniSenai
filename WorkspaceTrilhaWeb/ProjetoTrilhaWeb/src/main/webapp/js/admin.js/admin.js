4b8cff072dc7ff119485eb8013add722681a2462
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-08-20 21:27:16 -0300

    Atualizando a OT 12 de BackEnd

---
+++ WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/webapp/js/admin.js/admin.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	
+    $("header").load("/ProjetoTrilhaWeb/pages/admin/general/header.html");
+    $("footer").load("/ProjetoTrilhaWeb/pages/admin/general/footer.html");
+});
+
