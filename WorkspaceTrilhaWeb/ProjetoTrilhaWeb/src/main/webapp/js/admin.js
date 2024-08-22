e56fa82c7301bd1490a5fef0dd155b021bf9739e
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-08-21 21:08:14 -0300

    OT 12 da trilha de Back-end FINALIZADA

--- WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/webapp/js/admin.js
+++ WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/webapp/js/admin.js
@@ -3,4 +3,3 @@ $(document).ready(function() {
     $("header").load("/ProjetoTrilhaWeb/pages/admin/general/header.html");
     $("footer").load("/ProjetoTrilhaWeb/pages/admin/general/footer.html");
 });
-
