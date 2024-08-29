17f77fb92571e481262c781cc53130460219ee27
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-08-28 21:41:43 -0300

    new

---
+++ WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/webapp/js/product.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+ COLDIGO.produto = new Object();
+ 
+ $(document).ready(function(){
+	 
+	 //Carrega as marcas registradas no BD no select do formulario de inserir
+	 COLDIGO.produto.carregarMarcas = function(){
+		 
+	 }
+	 
+	 
+ });
