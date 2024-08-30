fb92a26966068b6a08ba2feeb7b1eec4363df1ef
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-08-29 21:34:06 -0300

    Atualizando a OT 16 cadastro Rest BackEnd

--- WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/webapp/js/product.js
+++ WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/webapp/js/product.js
@@ -9,6 +9,15 @@
 	 //Carrega as marcas registradas no BD no select do formulario de inserir
 	 COLDIGO.produto.carregarMarcas = function(){
 		 
+		 $.ajax({
+			 type: "GET",
+			 url: "/ProjetoTrilhaWeb/rest/marca/buscar",
+			 success: function(){
+			 },
+			 error: function(){	 
+			 }
+		 });
+		 
 	 }
 	 
 	 
