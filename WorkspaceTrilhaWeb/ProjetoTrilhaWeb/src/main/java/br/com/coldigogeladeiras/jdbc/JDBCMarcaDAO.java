d2c54477b9ce2f6f842f11045293cae13017a8d7
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-09-03 21:45:32 -0300

    new

---
+++ WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/java/br/com/coldigogeladeiras/jdbc/JDBCMarcaDAO.java
@@ -0,0 +1,71 @@
+package br.com.coldigogeladeiras.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.coldigogeladeiras.jdbcinterface.MarcaDAO;
+import br.com.coldigogeladeiras.modelo.Marca;
+
+public class JDBCMarcaDAO implements MarcaDAO {
+
+	private Connection conexao;
+
+	public JDBCMarcaDAO(Connection conexao) {
+		this.conexao = conexao;
+	}
+
+	public List<Marca> buscar() {
+
+		// Criação da instrução SQL para busca de todas as marcas
+		String comando = "SELECT * FROM marcas";
+
+		// Criação de uma lista para armazenar cada marca encontrada
+		List<Marca> listMarcas = new ArrayList<Marca>();
+
+		// Criação do objeto marca com valor null(ou seja, sem instancia-lo)
+		Marca marca = null;
+
+		// Abertura do try-catch
+		try {
+
+			// Uso da conexão do banco para prepara-lo para uma instrução SQL
+			Statement stmt = conexao.createStatement();
+
+			// Execução da instrução criada previamente
+			// e armazenamento do resultado no objeto rs
+			ResultSet rs = stmt.executeQuery(comando);
+
+			// Enquanto houver uma proxima linha no resultado
+			while (rs.next()) {
+
+				// Criação de instancia da classe marca
+				marca = new Marca();
+
+				// Recebimento dos 2 dados retornados do BD para cada linha encontrada
+				int id = rs.getInt("id");
+				String nome = rs.getString("nome");
+
+				// Setando no objeto marca os valores encontrados
+				marca.setId(id);
+				marca.setNome(nome);
+
+				// Adução da instancia contida no objeto Marca na lista de marcas
+				listMarcas.add(marca);
+
+			}
+
+			// Caso alguma Exception seja gerada no try, recebe-a no objeto "ex"
+		} catch (Exception ex) {
+			// Exibe a exceção na console
+			ex.printStackTrace();
+		}
+
+		// Retorna para quem chamou o metodo a lista criada
+		return listMarcas;
+	}
+}
