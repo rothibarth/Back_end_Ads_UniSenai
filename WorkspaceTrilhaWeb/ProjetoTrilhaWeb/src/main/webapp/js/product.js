a79ae63983894c6079c2f337ae3af609ef74489e
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-09-16 21:33:52 -0300

    new

--- WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/webapp/js/product.js
+++ WorkspaceTrilhaWeb/ProjetoTrilhaWeb/src/main/webapp/js/product.js
@@ -4,26 +4,44 @@ $(document).ready(function(){
 	
 	//Carrega as marcas registradas no BD no select do formulario de inserir
 	COLDIGO.produto.carregarMarcas = function(){
-	
-	alert("Tentando buscar marcas");
-	
+		
 	 $.ajax({
 			 type: "GET",
 			 url: "/ProjetoTrilhaWeb/rest/marca/buscar",
 			 success: function(marcas){
-				 alert("Sucesso");
+				 
+				 if(marcas!=""){
+					 
+					 $("#selMarca").html("");
+					 var option = document.createElement("option");
+					 option.setAttribute ("value", "");
+					 option.innerHTML = ("Escolha");
+					 $("#selMarca").append(option);
+					 
+					 for (var i = 0; i < marcas.lenght; i++){
+						 
+						 var option = document.createElement("option");
+						 option.setAttribute ("value", marcas[i].id);
+						 option.innerHTML = (marcas[i].nome);
+						 $("#selMarca").append(option);
+						 
+					 }
+					 
+				 }else{
+					 
+					 $("#selMarca").html("");
+					 
+					 var option = document.createElement("option");
+					 option.setAttribute("value", "");
+					 option.innerHTML = ("Cadastre uma marca primeiro!");
+					 $("#selMarca").append(option);
+					 $("#selMarca").addClass("aviso");
+
+					 
+				 }
+				 
 			 },
-			 error: function(info){
-				 
-				 COLDIGO.exibirAviso("Erro ao buscar as marcas: " + info.status + " - " + info.statusText);
-				 
-				 $("#selMarca").html("");
-				 var option = document.createElement("option");
-				 option.setAttribute("value", "");
-				 option.innerHTML = ("Erro ao carregar marcas!");
-				 $("#selMarca").append(option);
-				 $("#selMarca").addClass("aviso");
-			 }
+			
 		 });
 	}
 	 
